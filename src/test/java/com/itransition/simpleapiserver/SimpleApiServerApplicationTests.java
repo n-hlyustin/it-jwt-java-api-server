3b0d7abc2d7904c5b0eb52242bdb8fcb11ae9ff6
Nikita Khlyustin <nikita.ser777@gmail.com>
2022-01-24 11:33:56 +0300

    test active profile rename

--- src/test/java/com/itransition/simpleapiserver/SimpleApiServerApplicationTests.java
+++ src/test/java/com/itransition/simpleapiserver/SimpleApiServerApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("integration-test")
+@ActiveProfiles("test")
 public class SimpleApiServerApplicationTests {
 
     @Test
