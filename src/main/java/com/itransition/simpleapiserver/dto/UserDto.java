9d4a2ecb167012ae95ef470f028c511ff84fdc47
Nikita Khlyustin <nikita.ser777@gmail.com>
2022-02-09 13:01:59 +0300

    UserService unit tests add

--- src/main/java/com/itransition/simpleapiserver/dto/UserDto.java
+++ src/main/java/com/itransition/simpleapiserver/dto/UserDto.java
@@ -1,9 +1,16 @@
 package com.itransition.simpleapiserver.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.*;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDto {
 
     private Integer id;
