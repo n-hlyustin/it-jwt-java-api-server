31fb64fac077a9ccfa555f991bebdaff1858aaf7
Nikita Khlyustin <nikita.ser777@gmail.com>
2022-02-18 13:49:43 +0300

    events module add

---
+++ events/src/test/java/com/itransition/events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itransition.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
